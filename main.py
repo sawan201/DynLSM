16791355dd9d717cbe30961c7d9c5d8b58a901c2
Ahmad Awan <saadahmadawan4@gmail.com>
2025-06-18 09:48:13 -0400

    Test by saad

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello world")
+def func(num):
+    if num < 0:
+        raise ValueError("Negative numbers are not allowed")
+    return num * num
 
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
+def main():
+    x = func(5)
+    print(f"The square of 5 is {x}")
+
+main()
