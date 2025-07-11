e298942cdd90c99a3a725bf8d0a073b5b635fd80
Ahmad Awan <saadahmadawan4@gmail.com>
2025-07-11 10:47:09 -0400

    run test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from line_profiler import load_stats, show_text
 import contextlib
 
 # 1. Load your .lprof data
-stats = load_stats('main.py.lprof')
+stats = load_stats('ramsay_test.py.lprof')
 
 # 2. Open a text file and redirect print output into it
-with open('profile_results.txt', 'w') as f, contextlib.redirect_stdout(f):
+with open('ramsay_results.txt', 'w') as f, contextlib.redirect_stdout(f):
     # show_text takes (timings, unit) and prints the table to stdout
     show_text(stats.timings, stats.unit)
 
