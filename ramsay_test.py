d86abea031d6698b744ac00f091949402373377b
purplebamboo2 <ramsaybarlow@outlook.com>
2025-07-15 15:48:21 -0400

    Simulation run file

--- ramsay_test.py
+++ ramsay_test.py
@@ -3,7 +3,7 @@ import simulation
 def main():
     # Initialize the simulation parameters
     T = 3  # Number of time points
-    n = 3   # Number of actors
+    n = 10   # Number of actors
     p = 1   # Latent space dimensions
     SigmaSq = 1.0  # Variance for the latent positions
     TauSq = 1.0  # Variance for the tau prior
@@ -14,22 +14,16 @@ def main():
     XiIn = 1.0  # Input effect parameter for the model
     NuOut = 0.0  # Output effect parameter for the model
     XiOut = 1.0  # Output effect parameter for the model
-    EtaIn = 1.0  # Input effect parameter for the model
-    EtaOut = 1.0  # Output effect parameter for the model
     BetaIn = 0.8  # Input effect parameter for the model
     BetaOut = 1.3  # Output effect parameter for the model
-    RandomWalkVariance = 100.0  # Variance for the random walk 
     DirichletFactor = 1200  # Factor for the Dirichlet prior 
-    model_type = "binary"  # Type of model (e.g., "binary")
-    InitType = "base"  # Initialization type for the model
-    NumberOfSamples = 1000  # Total MCMC sweeps
-    BurnIn = 100  # First draws to discard
-    simName = "test"
+    model_type = "binary"  # Type of model (e.g., "binary")    
 
-
-    sim = simulation.Simulation(T, n, p, SigmaSq, TauSq, ThetaTau, ThetaSigma, PhiSigma, NuIn, XiIn, NuOut, XiOut, EtaIn, EtaOut, BetaIn, BetaOut, RandomWalkVariance, DirichletFactor, model_type, InitType, NumberOfSamples, BurnIn, simName = simName)
+    sim = simulation.Simulation(T, n, p, SigmaSq, TauSq, ThetaTau, ThetaSigma, PhiSigma, NuIn, XiIn, NuOut, XiOut, BetaIn, BetaOut, DirichletFactor, model_type)
     
-    sim.run()  # Run the simulation
+    sim.run(simName = "AllFixed", numberOfSamples = 1000, burnIn = 100, initType = "truth", randomWalkVariance = 100.0, fixX = False, fixR = False, fixBetaIN = False, fixBetaOUT = False, fixSigmaSq = False, fixTauSq = False)
+
+
 
 if __name__ == "__main__":
     main()  # Execute the main function when the script is run
